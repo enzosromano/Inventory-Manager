24e7264c76ca539f0ad5c242add5a674b1c1fbf7
enzosromano <enzo.romano@omicronmedia.com>
2021-07-18 15:49:33 -0400

    New methods for file import/export Fixed a bug where changing a name would change the serial number Changed input validation requirements Added file options to my menu chooser when saving

--- src/main/java/ucf/assignments/MainWindowController.java
+++ src/main/java/ucf/assignments/MainWindowController.java
@@ -5,8 +5,16 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.FileChooser;
+import javafx.util.converter.BigDecimalStringConverter;
+import javafx.util.converter.DoubleStringConverter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 public class MainWindowController {
@@ -17,27 +25,113 @@ public class MainWindowController {
     public TextField ItemNameField;
     public TextField ItemPriceField;
     public TextArea ErrorOutput;
+    public TableColumn SerialNumberColumn;
+    public TableColumn NameColumn;
+    public TableColumn PriceColumn;
+
 
     ItemHolder holder = new ItemHolder();
     MainWindowMethods methods = new MainWindowMethods();
 
     public void initialize() {
-        itemTable.setEditable(true);
 
-        TableColumn<Item, String> SerialNumberColumn = new TableColumn<>("Serial Number");
-        SerialNumberColumn.setMinWidth(150);
         SerialNumberColumn.setCellValueFactory(new PropertyValueFactory<>("serialNumber"));
-
-        TableColumn<Item, String> NameColumn = new TableColumn<>("Name");
-        NameColumn.setMinWidth(150);
         NameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-
-        TableColumn<Item, BigDecimal> PriceColumn = new TableColumn<>("Price");
-        PriceColumn.setMinWidth(150);
         PriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
 
         itemTable.setItems(ItemHolder.itemList);
-        itemTable.getColumns().addAll(SerialNumberColumn, NameColumn, PriceColumn);
+        itemTable.setEditable(true);
+        SerialNumberColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        NameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        PriceColumn.setCellFactory(TextFieldTableCell.forTableColumn(new BigDecimalStringConverter(){
+            @Override
+            public BigDecimal fromString(String value) {
+                try {
+                    return super.fromString(value);
+                } catch(NumberFormatException e) {
+                    ErrorOutput.setText("Price: Must be a decimal value");
+                    return null;
+                }
+            }
+        }));
+    }
+
+    public void changeSerialNumberCellEvent(TableColumn.CellEditEvent editedCell){
+
+        Item itemSelected = itemTable.getSelectionModel().getSelectedItem();
+        String newSerial = editedCell.getNewValue().toString();
+        if(methods.verifySerialNumber(newSerial)) {
+            itemSelected.setSerialNumber(newSerial);
+        }
+        else{
+            ErrorOutput.setText("Serials: Combination of 10 letters/numbers (No Duplicates!)");
+            System.out.println(itemSelected.getSerialNumber());
+            initialize();
+        }
+
+    }
+
+    public void changeNameCellEvent(TableColumn.CellEditEvent editedCell){
+
+        Item itemSelected = itemTable.getSelectionModel().getSelectedItem();
+        String newName = editedCell.getNewValue().toString();
+        if(methods.verifyName(newName)) {
+            itemSelected.setName(newName);
+        }
+        else{
+            ErrorOutput.setText("Names: 2-256 Characters");
+            initialize();
+        }
+
+    }
+
+    public void changePriceCellEvent(TableColumn.CellEditEvent editedCell){
+
+        Item itemSelected = itemTable.getSelectionModel().getSelectedItem();
+        BigDecimal newPrice = (BigDecimal) editedCell.getNewValue();
+        if (methods.verifyPrice(newPrice)) {
+            itemSelected.setPrice(newPrice);
+            System.out.println(itemSelected.getPrice());
+        } else {
+            ErrorOutput.setText("Price: Must be a decimal value");
+            initialize();
+        }
+
+
+    }
+
+    public void saveAs(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JSON files (*.json)", "*.json"),
+                new FileChooser.ExtensionFilter("HTML files (*.html)", "*.html"));
+
+        File toSave = fileChooser.showSaveDialog(null);
+        if(!methods.exportFile(toSave)){
+            ErrorOutput.setText("Could not export");
+        }
+    }
+
+    public void importFile(){
+        FileChooser fileChooser = new FileChooser();
+        File toImport = fileChooser.showOpenDialog(null);
+
+        String fileName = toImport.getName();
+        System.out.println(fileName);
+        String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1, toImport.getName().length());
+        System.out.println(">> fileExtension" + fileExtension);
+
+
+        if(fileExtension.equalsIgnoreCase("json")){
+            if(!methods.importJson(toImport)){
+                ErrorOutput.setText("Could not import JSON file!");
+            }
+        }
+        else if(fileExtension.equalsIgnoreCase("html")){
+
+        }
+
     }
 
     public void makeNewTask() {
@@ -78,6 +172,5 @@ public class MainWindowController {
         }
 
     }
-
 
 }
--- src/main/java/ucf/assignments/MainWindowMethods.java
+++ src/main/java/ucf/assignments/MainWindowMethods.java
@@ -1,6 +1,13 @@
 package ucf.assignments;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
 import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MainWindowMethods {
 
@@ -14,17 +21,24 @@ public class MainWindowMethods {
         if(itemSerialNumber.length() != 10){
             return false;
         }
-        /*for(String serialNum: ItemHolder.Serials){
+        for(String serialNum: ItemHolder.Serials){
             if(serialNum.equalsIgnoreCase(itemSerialNumber)){
                 return false;
             }
-        }*/
+        }
         for(int i = 0; i < itemSerialNumber.length(); i++){
             if(!Character.isLetter(itemSerialNumber.charAt(i)) && !Character.isDigit(itemSerialNumber.charAt(i))){
                 return false;
             }
         }
         return true;
+    }
+
+    public boolean verifyPrice(BigDecimal itemPrice){
+        if(itemPrice != null){
+            return true;
+        }
+        return false;
     }
 
 
@@ -42,6 +56,124 @@ public class MainWindowMethods {
 
     public boolean deleteItemMethod(Item toDelete){
         ItemHolder.itemList.remove(toDelete);
+        return true;
+    }
+
+    public boolean exportFile(File toSave){
+        if (toSave != null) {
+            String fileName = toSave.getName();
+            System.out.println(fileName);
+            String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1, toSave.getName().length());
+            System.out.println(">> fileExtension" + fileExtension);
+
+            try {
+                if (toSave.createNewFile()) {
+                    System.out.println("File created: " + toSave.getName());
+                } else {
+                    System.out.println("File already exists.");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
+            if(fileExtension.equalsIgnoreCase("json")){
+                return exportToJson(toSave);
+            }
+            else if(fileExtension.equalsIgnoreCase("html")){
+                return exportToHtml(toSave);
+            }
+
+        }
+
+        return true;
+
+    }
+
+    public boolean importJson(File toImport){
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(toImport.getPath()));
+
+            JSONObject jsonObject = (JSONObject) obj;
+            //Tasks must be stored within a JSON object of the value "tasks"
+            JSONArray itemList = (JSONArray) jsonObject.get("items");
+
+            for (JSONObject object : (Iterable<JSONObject>) itemList) {
+                BigDecimal price = new BigDecimal(object.get("price").toString());
+                ItemHolder.itemList.add(new Item(price, object.get("serialNumber").toString(),
+                        object.get("name").toString()));
+            }
+
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Uh Oh");
+        return false;
+    }
+
+    public boolean exportToJson(File toSave){
+
+        if(ItemHolder.itemList.isEmpty()){
+            return false;
+        }
+
+        //Create an array to store our tasks in and then an object to format it for our JSON output
+        JSONArray allItems = new JSONArray();
+        JSONObject toJson = new JSONObject();
+        for (int i = 0; i < ItemHolder.itemList.size(); i++) {
+            //Store each task in an abject so we can add it to our array of objects
+            JSONObject currentItem = new JSONObject();
+            currentItem.put("price", ItemHolder.itemList.get(i).getPrice());
+            currentItem.put("serialNumber", ItemHolder.itemList.get(i).getSerialNumber());
+            currentItem.put("name", ItemHolder.itemList.get(i).getName());
+            allItems.add(currentItem);
+        }
+        toJson.put("items", allItems);
+
+        //Put our output in string form for writing to the file
+        String jsonString = toJson.toString();
+        try {
+            FileWriter myWriter = new FileWriter(toSave);
+            myWriter.write(jsonString);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean exportToHtml(File toSave) {
+
+        if(ItemHolder.itemList.isEmpty()){
+            return false;
+        }
+
+        try {
+            PrintWriter writer = new PrintWriter(toSave);
+            StringBuilder outputString = new StringBuilder();
+            for (int i = 0; i < ItemHolder.itemList.size(); i++) {
+                Item currentItem = ItemHolder.itemList.get(i);
+
+                outputString.append(currentItem.getPrice() + " ");
+                outputString.append(currentItem.getSerialNumber() + " ");
+                outputString.append(currentItem.getName());
+                outputString.append("<br>");
+            }
+            writer.write(String.valueOf(outputString));
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+
         return true;
     }
 
